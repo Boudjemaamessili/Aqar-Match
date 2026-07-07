cd00314ccb05f6893e8a0eff806dd94c48fc4f81
Your Name <you@example.com>
2026-07-07 23:57:27 +0100

    Clean next config

--- Desktop/aqar-match-source/next.config.ts
+++ Desktop/aqar-match-source/next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-  allowedDevOrigins: ["*.space-z.ai"],
 };
 
 export default nextConfig;
